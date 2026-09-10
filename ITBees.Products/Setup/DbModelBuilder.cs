62eb9d35df93c4748519a550f462c15f189427a3
Jakub Zohar Rutkowski <kuba@skb.pl>
2026-09-10 19:37:30 +0200

    Separated to lib

--- ITBees.Products/Setup/DbModelBuilder.cs
+++ ITBees.Products/Setup/DbModelBuilder.cs
@@ -6,13 +6,18 @@ namespace ITBees.Products.Setup;
 
 public class DbModelBuilder
 {
+    /// <summary>
+    /// Registers every entity shipped with this package. Call it from the consuming
+    /// DbContext's OnModelCreating - the host must not repeat these mappings itself.
+    /// </summary>
     public static void Register(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Warehouse>().HasKey(x => x.Guid);
         modelBuilder.Entity<Producer>().HasKey(x => x.Id);
+        modelBuilder.Entity<Product>().HasKey(x => x.Id);
+        modelBuilder.Entity<ProductImage>().HasKey(x => x.Id);
         modelBuilder.Entity<SimCard>().HasKey(x => x.Id);
         modelBuilder.Entity<SimCardOperator>().HasKey(x => x.Id);
         modelBuilder.Entity<SerializedProductOnStock>().HasKey(x => x.Id);
-       
     }
 }
